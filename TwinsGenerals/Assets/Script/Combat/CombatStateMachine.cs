7dcd6bbb1b4be33cb098e5aeb30f0c8b4d658876
Eduardo <eduardo.straub1@gmail.com>
2015-06-19 00:51:58 -0300

    Added combat
    incomplete combat anyway.

---
+++ TwinsGenerals/Assets/Script/Combat/CombatStateMachine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatStateMachine : MonoBehaviour
+{
+    public enum BattleStates
+    {
+        START,
+        PLAYERINTERRUPT,
+        LOSE,
+        WIN,
+        TIE
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
