1de776554a91f1713d6e7fd8d9db4022b9cf2e5a
Eduardo Straub <eduardo.straub1@gmail.com>
2015-06-25 14:56:49 -0300

    CombatStatedMachine improved.

--- TwinsGenerals/Assets/Script/Combat/CombatStateMachine.cs
+++ TwinsGenerals/Assets/Script/Combat/CombatStateMachine.cs
@@ -6,7 +6,9 @@ public class CombatStateMachine : MonoBehaviour
     public enum BattleStates
     {
         START,
-        PLAYERINTERRUPT,
+        INTERRUPT_COMMANDS,
+        ATTACKER_ANIMATION,
+        DEFENDER_ANIMATION,
         LOSE,
         WIN,
         TIE
@@ -17,12 +19,30 @@ public class CombatStateMachine : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        currentstate = BattleStates.START;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(currentstate);
+        switch (currentstate)
+        {
+            case(BattleStates.START):
+                currentstate = BattleStates.ATTACKER_ANIMATION;
+                break;
+            case (BattleStates.INTERRUPT_COMMANDS):
+                break;
+            case (BattleStates.ATTACKER_ANIMATION):
+                break;
+            case (BattleStates.DEFENDER_ANIMATION):
+                break;
+            case (BattleStates.TIE):
+                break;
+            case (BattleStates.LOSE):
+                break;
+            case (BattleStates.WIN):
+                break;
+        }
     }
 }
