911c5291b6d86613f07d65dbba40c8feeef408b1
Eduardo Straub <eduardo.straub1@gmail.com>
2015-06-26 22:50:31 -0300

    Added BaseStatus

--- TwinsGenerals/Assets/Script/Characters/BaseCharacter.cs
+++ TwinsGenerals/Assets/Script/Characters/BaseCharacter.cs
@@ -7,6 +7,19 @@ public class BaseCharacter
     private int level;
     private BaseClass clase;
     private BaseAlignment current_alignment;
+    private BaseStatus current_status;
+
+    public BaseStatus Current_Status
+    {
+        get { return current_status; }
+        set { current_status = value; }
+    }
+    public BaseAlignment Current_Alignment
+    {
+        get { return current_alignment; }
+        set { current_alignment = value; }
+    }
+    
 
     public BaseClass Clase
     {
---
+++ TwinsGenerals/Assets/Script/Status/BaseStatus.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseStatus : MonoBehaviour
+{
+    private Status status;
+    private string status_effect;
+
+    public string Status_effect
+    {
+        get { return status_effect; }
+        set { status_effect = value; }
+    }
+    public Status Status1
+    {
+        get { return status; }
+        set { status = value; }
+    }
+    
+    public enum Status
+    {
+        Normal,
+        Paralyze,
+        Frozen,
+        Burn,
+        Sleep,
+        Attack_Up,
+        Attack_Down,
+        Defence_Up,
+        Defence_Down
+    }
+}
